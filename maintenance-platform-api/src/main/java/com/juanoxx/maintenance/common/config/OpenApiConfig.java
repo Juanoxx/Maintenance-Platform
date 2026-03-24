5dc42ad53b6cc53de309d11089fbd01f11b11e85
Juan Arredondo <juan.arredondo.c@usach.cl>
2026-03-24 20:08:12 -0300

    build: Configure JWT security scheme for OpenAPI documentation

--- maintenance-platform-api/src/main/java/com/juanoxx/maintenance/common/config/OpenApiConfig.java
+++ maintenance-platform-api/src/main/java/com/juanoxx/maintenance/common/config/OpenApiConfig.java
@@ -1,17 +1,30 @@
 package com.juanoxx.maintenance.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String BEARER_SCHEME = "bearerAuth";
+
     @Bean
     public OpenAPI maintenanceOpenApi() {
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes(
+                        BEARER_SCHEME,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
                 .info(new Info()
                         .title("Maintenance Platform API")
                         .description("API for building maintenance incident lifecycle management")
