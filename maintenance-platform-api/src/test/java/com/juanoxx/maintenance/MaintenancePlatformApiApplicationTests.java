457d4a6832e7c6104517a115d07e86f6f7e0bcad
Juan Arredondo <juan.arredondo.c@usach.cl>
2026-03-24 19:06:37 -0300

    test: Add basic Spring Boot test infrastructure

---
+++ maintenance-platform-api/src/test/java/com/juanoxx/maintenance/MaintenancePlatformApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.juanoxx.maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MaintenancePlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
